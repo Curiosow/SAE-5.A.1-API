cc444b77d1bbcbbd31043361e96130b4fff7c3f3
Curiosow <teampandapro45@gmail.com>
2025-11-26 22:56:39 +0100

    Update CorsConfig to allow CORS requests from localhost and a specific origin

--- src/main/java/fr/uphf/sae5a1api/configuration/CorsConfig.java
+++ src/main/java/fr/uphf/sae5a1api/configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://oscarapi.eternyuhc.fr")
+                        .allowedOrigins("http://localhost:3000", "https://oscarapi.eternyuhc.fr")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
